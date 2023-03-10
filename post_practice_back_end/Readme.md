423b42ef4331b3353f6ea2088855bcba58ffb5b3
asmallick1999 <asmallick1999@gmail.com>
2023-03-10 11:27:57 +0530

    added readme.md file in backend

---
+++ post_practice_back_end/Readme.md
@@ -0,0 +1,20 @@
+Well, this is a normal backend server where I created "POST" and "GET" API using Express.Js
+
+In POST api I send (name, email, mobile no) data from React
+and in GET I fetch those (name, email, mobile no) data.
+
+
+# How to run this Back-End
+
+## 1st Step
+
+You need to install npm using ->
+
+### `npm i`
+
+## 2nd Step
+
+To run this project you have to use supervisor
+like -> 
+
+### `supervisor app.js`
--- post_practice_front_end/src/HomePage.js
+++ post_practice_front_end/src/HomePage.js
@@ -3,34 +3,41 @@ import { useState, useEffect } from 'react';
 
 function HomePage() {
     const [details, setDetails] = useState([]);
-    useEffect(()=>{
-        fetch('http://localhost:4500/details/all',{method:'GET'}).then((result)=>{
-            result.json().then((resp)=>{
+    useEffect(() => {
+        fetch('http://localhost:4500/details/all', { method: 'GET' }).then((result) => {
+            result.json().then((resp) => {
                 setDetails(resp)
             })
         })
-    },[])
-    
-  return (
-    <div>
-        <table>
-            <tr>
-                <td>Name</td>
-                <td>Email</td>
-                <td>Mobile No.</td>
-            </tr>
-            {
-                details.map(item=>
-                   <tr>
-                        <td>{item.name}</td>
-                        <td>{item.email}</td>
-                        <td>{item.mobile}</td>
-                   </tr> 
-                    )
-            }
-        </table>
-    </div>
-  )
+    }, [])
+
+    return (
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <td>Name</td>
+                        <td>Email</td>
+                        <td>Mobile No.</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        details.map(item =>
+                            <tr>
+                                <td>{item.name}</td>
+                                <td>{item.email}</td>
+                                <td>{item.mobile}</td>
+                            </tr>
+                        )
+                    }
+
+                </tbody>
+
+
+            </table>
+        </div>
+    )
 }
 
 export default HomePage;
